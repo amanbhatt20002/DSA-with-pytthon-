935ee5710f38b157aee78109eb85ba8cc6349b04
aman bhatt <amanb3588@gmail.com>
2025-11-22 09:12:17 +0530

    second largest el  in array

--- largestEl.py
+++ largestEl.py
@@ -8,5 +8,23 @@ def largestEl(arr):
 
 
 
-arr=[1,2,3,4]
+
+
+
+
+# second Largest el in array
+def secondLEl(arr):
+    if len(arr) < 2:
+        return None  # no second largest
+    Sl=l=float('-inf')
+    for i in arr:
+        if i>l:
+            Sl=l
+            l=i
+        elif i> Sl and i!=l:
+            Sl=i
+    return Sl
+
+arr = [5, 1, 2, 3]
 print(largestEl(arr))
+print(secondLEl(arr))
