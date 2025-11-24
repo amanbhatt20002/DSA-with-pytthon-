f1ad62270cdf6debf52f384591f3d73797587e5f
aman bhatt <amanb3588@gmail.com>
2025-11-24 15:14:08 +0530

    linking  all nodes

--- linkedlist/node.py
+++ linkedlist/node.py
@@ -12,8 +12,15 @@ node3=Node(7)
 node4=Node(8)
 node5=Node(3)
 
+node1.next=node2
+node2.next=node3
+node3.next=node4
+node4.next=node5
 print(node1.next)
 print(node2.next)
 print(node3.value)
 print(node4.value)
 print(node5.value)
+
+print("..............................................................................")
+print(node1.next.next.next.next.value)
