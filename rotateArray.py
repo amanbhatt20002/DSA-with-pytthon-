fca3fd01fe88613829262d904bfe74bdfd896ad0
aman bhatt <amanb3588@gmail.com>
2025-11-24 09:08:39 +0530

    rotated array  optize solution

--- rotateArray.py
+++ rotateArray.py
@@ -12,3 +12,21 @@ def rotateBrute(arr):
     return arr
 
 print(rotateBrute([1,2,3,4]))
+
+
+# optimise solution 
+def reverse(arr,left,right):
+    while left<right:
+        arr[left],arr[right]=arr[right],arr[left]
+        left+=1
+        right-=1
+
+arr=[1,2,3,4,5]
+n=len(arr)
+k = 1
+k = k % n 
+print(arr)
+reverse(arr,n-k,n-1)
+reverse(arr,0,n-k-1)
+reverse(arr,0,n-1)
+print(arr)
